f6c3313f82e697e74b897f271e85e31beeea51f0
Vitalii Morozov <moroemail@gmail.com>
2018-01-28 20:27:41 +0200

    Fixed typo, last lib updates

--- app/src/main/java/com/spacelobster/fuckingbills/FuckingBillsApplication.kt
+++ app/src/main/java/com/spacelobster/fuckingbills/FuckingBillsApplication.kt
@@ -6,7 +6,7 @@ import io.realm.Realm
 /**
  * Created by spacelobster on 8/16/17.
  */
-class FuckingBillsAppication : Application() {
+class FuckingBillsApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
